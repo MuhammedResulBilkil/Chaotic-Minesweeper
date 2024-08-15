2f967fd8020a59ddf1989ee5ed4af96883c24130
Resul <garnnet_221@hotmail.com>
2024-08-15 21:41:18 +0300

    RichText implemented

---
+++ Source/Minesweeper/GeneralUIWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GeneralUIWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINESWEEPER_API UGeneralUIWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
