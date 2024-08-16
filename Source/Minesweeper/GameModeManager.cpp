a677df85318aa3064de6d0cd84881d7146a0740a
Resul <garnnet_221@hotmail.com>
2024-08-16 23:21:24 +0300

    UMG Widget OnValueChanged added in GameModeManager!

---
+++ Source/Minesweeper/GameModeManager.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameModeManager.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Components/Slider.h"
+#include "Components/TextBlock.h"
+
+void AGameModeManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Create viewport
+	if (GeneralUI)
+	{
+		GeneralUIWidget = CreateWidget<UUserWidget>(GetWorld(), GeneralUI);
+		
+		if (GeneralUIWidget)
+		{
+			
+			USlider* GridAsSquareSlider = (USlider*) GeneralUIWidget->GetWidgetFromName(TEXT("Slider_GridAsSquare"));
+			GridAsSquareText = (UTextBlock*) GeneralUIWidget->GetWidgetFromName(TEXT("Text_GridAsSquare"));
+			
+			if(GridAsSquareSlider)
+				GridAsSquareSlider->OnValueChanged.AddDynamic(this, &AGameModeManager::GridAsSquareSliderValueChanged);
+			
+			GeneralUIWidget->AddToViewport();
+		}
+	}
+	
+}
+
+void AGameModeManager::GridAsSquareSliderValueChanged(float Value)
+{
+	//log
+	UE_LOG(LogTemp, Warning, TEXT("GridAsSquareSliderValueChanged: %f"), Value);
+
+	GridAsSquareText->SetText(FText::FromString(FString::Printf(TEXT("Grid as Square: %d"), (int)Value)));
+}
---
+++ Source/Minesweeper/GameModeManager.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameModeManager.generated.h"
+
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class MINESWEEPER_API AGameModeManager : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
+	TSubclassOf<UUserWidget> GeneralUI;
+
+private:
+
+	UPROPERTY()
+	UUserWidget* GeneralUIWidget;
+
+	UPROPERTY()
+	UTextBlock* GridAsSquareText;
+
+	UFUNCTION()
+	void GridAsSquareSliderValueChanged(float Value);
+};
